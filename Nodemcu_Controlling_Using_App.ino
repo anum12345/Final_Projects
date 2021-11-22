2b6b27b94b9af242ab98f871e49514d8557b84be
anum12345 <87125417+anum12345@users.noreply.github.com>
2021-11-22 08:14:34 -0800

    Update Nodemcu_Controlling_Using_App.ino

--- Nodemcu_Controlling_Using_App.ino
+++ Nodemcu_Controlling_Using_App.ino
@@ -1,1 +1,24 @@
-#include <ESP8266WiFi.h>
+
+#include<ESP8266WiFi.h>
+string i;
+WiFiServer server(80);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(4,OUTPUT);
+  WiFi.disconnect();
+  delay(300);
+  WiFi.begin("moazam","moazam123");
+  Serial.println("Connected to Wifi");
+  while(WiFi.status() != WL_CONNECTED)
+    {
+    Serial.print("..");
+    delay(200);
+  }
+  Serial.println();
+  Serial.println("NodeMCU is Connected!");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
+
+}
