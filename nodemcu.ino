d4304c7567101431c0770e5dfb2c8d728a85c35b
anum12345 <87125417+anum12345@users.noreply.github.com>
2021-11-22 05:38:31 -0800

    Update nodemcu.ino

--- nodemcu.ino
+++ nodemcu.ino
@@ -1,1 +1,61 @@
-sd
+#define fan 2
+#define bulb 3
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(fan, OUTPUT);
+  pinMode(bulb, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() == 1)
+  {
+    String val = Serial.readString();
+    Serial.println(val);
+    if(val == "fan on")
+    {
+      digitalWrite(fan, HIGH);
+    }
+    if(val == "fan off")
+    {
+      digitalWrite(fan, LOW);
+    }
+    if(val == "room bulb on")
+    {
+      digitalWrite(bulb, HIGH);
+    }
+    if(val == "room bulb off")
+    {
+      digitalWrite(bulb, LOW);
+    }
+    if(val == "kitchen bulb on")
+    {
+      digitalWrite(bulb, HIGH);
+    }
+    if(val == "kitchen bulb off")
+    {
+      digitalWrite(bulb, LOW);
+    }
+    if(val == "controller room bulb on")
+    {
+      digitalWrite(bulb, HIGH);
+    }
+    if(val == "controller room bulb off")
+    {
+      digitalWrite(bulb, LOW);
+    }
+    if(val == "all on")
+    {
+      digitalWrite(fan, HIGH);
+      digitalWrite(bulb, HIGH);
+    }
+    if(val == "all off")
+    {
+      digitalWrite(bulb, LOW);
+      digitalWrite(fan, LOW);
+    }
+  }
+}
+
