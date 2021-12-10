6dc6dbe9d5796648df7c93b564a422a2f006c8a1
anum12345 <87125417+anum12345@users.noreply.github.com>
2021-12-10 01:49:56 -0800

    Update ardruino_bluetooth.ino

--- ardruino_bluetooth.ino
+++ ardruino_bluetooth.ino
@@ -1,9 +1,9 @@
 #define RelayPin1 2 //room 
 #define RelayPin2 3  //kitchen 
-#define RelayPin3 4 //controller room 
+// #define RelayPin3 4 //controller room 
 #define Speed1 5
-#define Speed2 6
-#define Speed4 7
+// #define Speed2 6
+// #define Speed4 7
 
 
 void setup()
@@ -13,18 +13,18 @@ void setup()
   Serial.begin(9600);
   pinMode(RelayPin1, OUTPUT);
   pinMode(RelayPin2, OUTPUT);
-  pinMode(RelayPin3, OUTPUT);
+//   pinMode(RelayPin3, OUTPUT);
   digitalWrite(RelayPin1, HIGH);
   digitalWrite(RelayPin2, HIGH);
-  digitalWrite(RelayPin3, HIGH);
+//   digitalWrite(RelayPin3, HIGH);
   // Debug console
   pinMode(Speed1, OUTPUT);
-  pinMode(Speed2, OUTPUT);
-  pinMode(Speed4, OUTPUT);
+//   pinMode(Speed2, OUTPUT);
+//   pinMode(Speed4, OUTPUT);
   //Initially the fan will be in OFF state
   digitalWrite(Speed1, HIGH);
-  digitalWrite(Speed2, HIGH);
-  digitalWrite(Speed4, HIGH);
+//   digitalWrite(Speed2, HIGH);
+//   digitalWrite(Speed4, HIGH);
   
 }
 
@@ -51,14 +51,14 @@ void loop() {
     {
       digitalWrite(3, HIGH);
     }
-    if(val == "controller room bulb on")
-    {
-      digitalWrite(4, LOW);
-    }
-    if(val == "controller room bulb off")
-    {
-      digitalWrite(4, HIGH);
-    }
+//     if(val == "controller room bulb on")
+//     {
+//       digitalWrite(4, LOW);
+//     }
+//     if(val == "controller room bulb off")
+//     {
+//       digitalWrite(4, HIGH);
+//     }
    if(val="all bulb on")
    {
     digitalWrite(2, LOW);
@@ -76,55 +76,63 @@ void loop() {
   {
     //All realys Off - Fan Off
     
+    digitalWrite(Speed1, HIGH);
+//     digitalWrite(Speed2, LOW);
+//     digitalWrite(Speed4, LOW);
+   }
+     if (val== "fan on")
+  {
+    //All realys Off - Fan Off
+    
     digitalWrite(Speed1, LOW);
-    digitalWrite(Speed2, LOW);
-    digitalWrite(Speed4, LOW);
-  }
+//     digitalWrite(Speed2, LOW);
+//     digitalWrite(Speed4, LOW);
+//    }
 
-  if (val == "fan speed one ")
-  {
-    //Speed1 Relay On - Fan at speed 1
+//   if (val == "fan speed one ")
+//   {
+//     //Speed1 Relay On - Fan at speed 1
     
-    digitalWrite(Speed1, HIGH);
-    digitalWrite(Speed2, HIGH);
-    digitalWrite(Speed4, HIGH);
-    delay(500);
-    digitalWrite(Speed1, LOW);
-  }
+//     digitalWrite(Speed1, HIGH);
+//     digitalWrite(Speed2, HIGH);
+//     digitalWrite(Speed4, HIGH);
+//     delay(500);
+//     digitalWrite(Speed1, LOW);
+//   }
 
-  if (val="fan speed two ")
-  {
-    //Speed2 Relay On - Fan at speed 2
+//   if (val="fan speed two ")
+//   {
+//     //Speed2 Relay On - Fan at speed 2
     
-    digitalWrite(Speed1, HIGH);
-    digitalWrite(Speed2, HIGH);
-    digitalWrite(Speed4, HIGH);
-    delay(500);
-    digitalWrite(Speed2, LOW);
-  }
+//     digitalWrite(Speed1, HIGH);
+//     digitalWrite(Speed2, HIGH);
+//     digitalWrite(Speed4, HIGH);
+//     delay(500);
+//     digitalWrite(Speed2, LOW);
+//   }
 
-  if (val="fan speed three")
-  {
-    //Speed1 & Speed2 Relays On - Fan at speed 3
+//   if (val="fan speed three")
+//   {
+//     //Speed1 & Speed2 Relays On - Fan at speed 3
     
-    digitalWrite(Speed1, HIGH);
-    digitalWrite(Speed2, HIGH);
-    digitalWrite(Speed4, HIGH);
-    delay(500);
-    digitalWrite(Speed1, LOW);
-    digitalWrite(Speed2, LOW);
-  }
+//     digitalWrite(Speed1, HIGH);
+//     digitalWrite(Speed2, HIGH);
+//     digitalWrite(Speed4, HIGH);
+//     delay(500);
+//     digitalWrite(Speed1, LOW);
+//     digitalWrite(Speed2, LOW);
+//   }
 
-  if (val="fan speed four")
-  {
-    //Speed4 Relay On - Fan at speed 4
+//   if (val="fan speed four")
+//   {
+//     //Speed4 Relay On - Fan at speed 4
     
-    digitalWrite(Speed1, HIGH);
-    digitalWrite(Speed2, HIGH);
-    digitalWrite(Speed4, HIGH);
-    delay(500);
-    digitalWrite(Speed4, LOW);
-  }
+//     digitalWrite(Speed1, HIGH);
+//     digitalWrite(Speed2, HIGH);
+//     digitalWrite(Speed4, HIGH);
+//     delay(500);
+//     digitalWrite(Speed4, LOW);
+//   }
   
 }
 }
